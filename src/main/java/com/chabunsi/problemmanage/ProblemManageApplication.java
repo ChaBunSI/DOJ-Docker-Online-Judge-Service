69b00cb8c07bda1d4f2d04f5521d1514ab300a59
강병우 <peace0096@naver.com>
2023-11-05 20:29:02 +0900

    Repo Init

---
+++ src/main/java/com/chabunsi/problemmanage/ProblemManageApplication.java
@@ -0,0 +1,13 @@
+package com.chabunsi.problemmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chabunsi/problemmanage/ProblemManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chabunsi.problemmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
