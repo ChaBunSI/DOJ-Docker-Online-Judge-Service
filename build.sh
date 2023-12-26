8240558805a52c11a3ed60ac46500f319c93603a
강병우 <peace0096@naver.com>
2023-12-26 19:05:57 +0900

    [추가] 빌드 자동화 및 도커 컴포즈 추가

--- build.sh
+++ build.sh
@@ -1,22 +1,17 @@
 # AWS Keys
-ACCESS_KEY=
-SECRET_KEY=
+AWS_ACCESS_KEY=
+AWS_SECRET_KEY=
 
 # SNS Settings
-JudgeCPP_TopicArn=arn:aws:sns:
-JudgeNotCPP_TopicArn=arn:aws:sns:
-JudgeRT_TopicArn=arn:aws:sns:
-JudgeTask_TopicArn=arn:aws:sns:
-ScoreQueue_TopicArn=arn:aws:sns:
-SubmissionDone_TopicArn=arn:aws:sns:
-ProblemManageQueue_TopicArn=arn:aws:sns:
+DOJ_Judge_JudgeDone_name=
+DOJ_Judge_JudgeDone_arn=
+DOJ_Submission_SubmitTask_name=
+DOJ_Submission_SubmitTask_arn=
+DOJ_TestCase_Queueing_name=
+DOJ_TestCase_Queueing_arn=
+
 
 # SQS Settings
-JudgeCPP_ARN=arn:aws:sqs:
-JudgeCPP_URL=https://sqs.
-
-JudgeNotCPP_ARN=arn:aws:sqs:
-JudgeNotCPP_URL=https://sqs.
 
 JudgeRT_ARN=arn:aws:sqs:
 JudgeRT_URL=https://sqs.
@@ -30,40 +25,81 @@ ScoreQueue_URL=https://sqs.
 SubmissionDone_ARN=arn:aws:sqs:
 SubmissionDone_URL=https://sqs.
 
+ProblemManageQueue_NAME=
 ProblemManageQueue_ARN=arn:aws:sqs:
 ProblemManageQueue_URL=https://sqs.
 
-# Docker Image Build
-cd ./ProblemManage
-docker-compose build
+# Create Env files
+echo "[STEP 1] Create ENV Files"
 
-# TODO : 환경변수 전달하는 파라미터 만들어줘야 함.
-cd ../DOJ_ms_submission
-docker-compose build
-
-cd ../AUTH-SERVICE
-docker build -t auth-service .
-
-cd ../CLIENT-SERVICE
-docker build -t client-service .
-
-cd ../AUTH-SERVICE
-docker build -t auth-service .
-
-cd ../DOSCOVERY-SERVICE
-docker build -t discovery-service .
-
-cd ../AUTH-SERVICE
-docker build -t auth-service .
-
-cd ../GATEWAY-SERVICE
-docker build -t gateway-service .
-
-cd ../AUTH-SERVICE
-docker build -t auth-service .
-
-cd ../CLIENT-SERVICE
-docker build -t client-service .
+cd ./DOJ_ms_submission
+rm .env
+echo "AWS_ACCESS_KEY=$AWS_ACCESS_KEY" >> .env
+echo "AWS_SECRET_KEY=$AWS_SECRET_KEY" >> .env
+echo "AWS_REGION=ap-northeast-2" >> .env
+echo "AWS_SQS_URL=1" >> .env
+echo "AWS_SNS_ARN_SUBMIT=$DOJ_Submission_SubmitTask_arn" >> .env
+echo "AWS_SNS_SUBJECT_SUBMIT=submit" >> .env
+echo "AWS_SNS_MSGGROUPID_SUBMIT=submit" >> .env
 
 cd ../RT-SERVICE
+rm .env
+echo "AWS_ACCESS_KEY=$AWS_ACCESS_KEY" >> .env
+echo "AWS_SECRET_KEY=$AWS_SECRET_KEY" >> .env
+echo "QUEUE_URL=$JudgeRT_URL" >> .env
+
+cd ../
+
+# Docker Image Build
+# build repositories
+echo "[STEP 2] Building repositories..."
+
+# build discovery image and create container
+cd DISCOVERY-SERVICE
+echo "[STEP 2] Building DISCOVERY-SERVICE..."
+docker build -t doj-discovery-service .
+
+# build gateway image and create container
+cd ../GATEWAY-SERVICE
+echo "[STEP 2] Building GATEWAY-SERVICE..."
+docker build -t doj-gateway-service .
+
+# build auth image and create container
+cd ../AUTH-SERVICE
+echo "[STEP 2] Building AUTH-SERVICE..."
+docker build -t doj-auth-service .
+
+# build submission image and create container
+cd ../DOJ_ms_submission
+echo "[STEP 2] Building DOJ_ms_submission..."
+docker build -t submission-service .
+
+
+# build rt image and create container
+cd ../RT-SERVICE
+echo "[STEP 2] Building RT-SERVICE..."
 docker build -t rt-service .
+
+
+# build ProblemManage image and create container
+cd ../ProblemManage
+echo "[STEP 2] Building ProblemManage..."
+docker build -t problem-manage-service \
+--build-arg access_key=$AWS_ACCESS_KEY \
+--build-arg secret_key=$AWS_<sqs_name>SECRET_KEY \
+--build-arg region=ap-northeast-2 \
+--build-arg topic_arn=$DOJ_TestCase_Queueing_arn \
+--build-arg sqs_name=$ProblemManageQueue_NAME \
+--build-arg sqs_url=$ProblemManageQueue_URL .
+
+# docker pull peace0096/proglram_manage_service:Submit
+# docker run -d -p 4000:3306 --name ProgramManageDB -e MARIADB_ROOT_PASSWORD=1234 --env MARIADB_DATABASE=ProblemManageDB  mariadb
+# docker run -d -p 8080:8080 --name ProblemManage-container --link ProgramManageDB:ProblemManageDB peace0096/program_manage_service:Submit
+
+
+# build client image and create container
+cd ../CLIENT-SERVICE
+echo "[STEP 2] Building CLIENT-SERVICE..."
+docker build -t client-service .
+
+
--- build_Judge.sh
+++ build_Judge.sh
@@ -1,35 +1,60 @@
 # AWS Keys
-ACCESS_KEY=
-SECRET_KEY=
+AWS_ACCESS_KEY=
+AWS_SECRET_KEY=
 
 # SNS Settings
-JudgeCPP_TopicArn=arn:aws:sns:
-JudgeNotCPP_TopicArn=arn:aws:sns:
-JudgeRT_TopicArn=arn:aws:sns:
-JudgeTask_TopicArn=arn:aws:sns:
-ScoreQueue_TopicArn=arn:aws:sns:
-SubmissionDone_TopicArn=arn:aws:sns:
-ProblemManageQueue_TopicArn=arn:aws:sns:
+DOJ_Judge_JudgeDone_NAME=DOJ-Judge-JudgeDone.fifo
+DOJ_Judge_JudgeDone_ARN=
 
 # SQS Settings
+JudgeCPP_NAME=JudgeCPP.fifo
 JudgeCPP_ARN=arn:aws:sqs:
 JudgeCPP_URL=https://sqs.
 
+JudgeNotCPP_NAME=JudgeNotCPP.fifo
 JudgeNotCPP_ARN=arn:aws:sqs:
 JudgeNotCPP_URL=https://sqs.
 
+JudgeRT_NAME=JudgeRT.fifo
 JudgeRT_ARN=arn:aws:sqs:
 JudgeRT_URL=https://sqs.
 
+JudgeTask_NAME=JudgeTask.fifo
 JudgeTask_ARN=arn:aws:sqs:
 JudgeTask_URL=https://sqs.
 
+ScoreQueue_NAME=ScoreQueue.fifo
 ScoreQueue_ARN=arn:aws:sqs:
 ScoreQueue_URL=https://sqs.
 
-SubmissionDone_ARN=arn:aws:sqs:
-SubmissionDone_URL=https://sqs.
+# Create Env File
+cd ./JUDGE-SERVICE
+rm ./judge_aws.h
 
-ProblemManageQueue_ARN=arn:aws:sqs:
-ProblemManageQueue_URL=https://sqs.
+echo "#ifndef JUDGE_AWS_H" >> judge_aws.h
+echo "#define JUDGE_AWS_H" >> judge_aws.h
+echo "#include <aws/core/Aws.h>" >> judge_aws.h
+echo "#include <aws/core/auth/AWSCredentials.h>" >> judge_aws.h
+echo "const Aws::String JT_QUEUE_NAME = \"$JudgeTask_NAME\";" >> judge_aws.h
+echo "const Aws::String JT_QUEUE_URL = \"$JudgeTask_URL\";" >> judge_aws.h
+echo "const Aws::String JT_QUEUE_ARN = \"$JudgeTask_ARN\";" >> judge_aws.h
+echo "const Aws::String RT_QUEUE_NAME = \"$JudgeRT_NAME\";" >> judge_aws.h
+echo "const Aws::String RT_QUEUE_URL = \"$JudgeRT_ARN\";" >> judge_aws.h
+echo "const Aws::String RT_QUEUE_ARN = \"$JudgeRT_ARN\";" >> judge_aws.h
+echo "const Aws::String TOPIC_NAME = \"$DOJ_Judge_JudgeDone_NAME\";" >> judge_aws.h
+echo "const Aws::String TOPIC_ARN = \"$DOJ_Judge_JudgeDone_ARN\";" >> judge_aws.h
+echo "const Aws::String CRUD_QUEUE_NAME = \"$ScoreQueue_NAME\";" >> judge_aws.h
+echo "const Aws::String CRUD_QUEUE_URL = \"$ScoreQueue_URL\";" >> judge_aws.h
+echo "const Aws::String CRUD_QUEUE_ARN = \"$ScoreQueue_ARN\";" >> judge_aws.h
+echo "const Aws::String CPP_QUEUE_NAME = \"$JudgeCPP_NAME\";" >> judge_aws.h
+echo "const Aws::String CPP_QUEUE_URL = \"$JudgeCPP_URL\";" >> judge_aws.h
+echo "const Aws::String CPP_QUEUE_ARN = \"$JudgeCPP_ARN\";" >> judge_aws.h
+echo "const Aws::String NotCPP_QUEUE_NAME = \"$JudgeNotCPP_NAME\";" >> judge_aws.h
+echo "const Aws::String NotCPP_QUEUE_URL = \"$JudgeNotCPP_URL\";" >> judge_aws.h
+echo "const Aws::String NotCPP_QUEUE_ARN = \"$JudgeNotCPP_ARN\";" >> judge_aws.h
 
+echo "const Aws::String ACCESS_KEY = \"$AWS_ACCESS_KEY\";" >> judge_aws.h
+echo "const Aws::String SECRET_KEY = \"$AWS_SECRET_KEY\";" >> judge_aws.h
+
+# Build Docker Image
+docker build -t jduge_service .
